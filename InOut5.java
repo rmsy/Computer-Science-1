b899d00c6ce8f1af9ecc2b6892d6aa802bd427e2
Isaac Moore <rmsy@me.com>
2012-09-22 15:59:21 -0500

    Create fifth submission, finish first assignment

---
+++ InOut5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class InOut5 { 
+  public static void main(String[] args) {
+    System.out.println("Please enter your first name, middle initial, and last name:");
+    String[] response;
+    response = new String[3];
+    Scanner keyboard = new Scanner(System.in);
+    response[0] = keyboard.next();
+    response[1] = keyboard.next();
+    response[2] = keyboard.next();
+    System.out.println("Name    : " + response[0] + " " + response[1] + ". " + response[2]);
+    System.out.println("Name    : " + response[2] + ", " + response[0] + " " + response[1] + ".");
+  }  
+}
