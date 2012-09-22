e96b8deb42ea6aa5e289a6bc1169cf56e7048294
Isaac Moore <rmsy@me.com>
2012-09-22 14:36:16 -0500

    Create second submission

---
+++ InOut2.java
@@ -0,0 +1,8 @@
+public class InOut2 {
+  public static void main(String[] args) { 
+    System.out.println("Isaac Moore");
+    System.out.println("1084 Late Ave. (Weatherford, OK, 73906)");
+    System.out.println("(682) 710-2247");
+    return;
+  }
+}
