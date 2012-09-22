5d0b50e18ee879033ba5e439f9e6e826eceb538f
Isaac Moore <rmsy@me.com>
2012-09-22 14:54:18 -0500

    Create third submission

---
+++ InOut3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class InOut3 {
+  public static void main(String[] args) {
+    Scanner keyboard = new Scanner(System.in);
+    System.out.println("Please enter three decimal numbers:");
+    double[] numbers;
+    numbers = new double[3];
+    numbers[0] = keyboard.nextDouble();
+    numbers[1] = keyboard.nextDouble();
+    numbers[2] = keyboard.nextDouble();
+    System.out.println("Your numbers were " + numbers[0] + ", " + numbers[1] + ", and " + numbers[2]);
+  }
+}
