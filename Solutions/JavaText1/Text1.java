ffff03228a9d02db1dd7ef0bbe348c9c7e86fb3f
Isaac Moore <rmsy@me.com>
2012-10-21 19:52:46 -0500

    Create Text2 solution and accompanying resources.

--- Solutions/JavaText1/Text1.java
+++ Solutions/JavaText1/Text1.java
@@ -22,8 +22,15 @@ public class Text1
     System.out.println("==============");
     try
     {
-      Thread.sleep(10000);
-    } catch(InterruptedException tooSoon) {}
-    System.exit(1);
+      file.close();
+    } catch(IOException closeError)
+    {
+      System.out.println("There was an error closing the file. It may still be open. Closing the environment to be safe...");
+      try
+      {
+        Thread.sleep(10000);
+      } catch(InterruptedException tooSoon) {}
+      System.exit(1);
+    }
   }
 }
--- Solutions/JavaText1/Text2.java
+++ Solutions/JavaText1/Text2.java
@@ -1,7 +1,32 @@
+import java.io.*;
+import java.util.*;
+
 public class Text2
-{  
+{
   public static void main(String[] args)
   {
-    
+    FileReader file = null;
+    try
+    {
+      file = new FileReader("D:\\Java\\Resources\\text2.dat");
+    } catch(FileNotFoundException fileNotFound)
+    {   
+      System.out.println("The file was not available.");
+      return;
+    }
+    Scanner fr = new Scanner(file);
+    System.out.printf("%12.4f\n%12.4f\n%12.4f\n%12.4f\n%12.4f\n", fr.nextDouble(), fr.nextDouble(), fr.nextDouble(), fr.nextDouble(), fr.nextDouble());
+    try
+    {
+      file.close();
+    } catch(IOException closeError)
+    {
+      System.out.println("There was an error closing the file. It may still be open. Closing the environment to be safe...");
+      try
+      {
+        Thread.sleep(10000);
+      } catch(InterruptedException tooSoon) {}
+      System.exit(1);
+    }
   }
 }
