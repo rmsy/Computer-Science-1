59c762f36eca88986613d6e89896ef91380a448f
Isaac Moore <rmsy@me.com>
2012-10-21 19:58:48 -0500

    Create Text3 solution and accompanying resources.

---
+++ Solutions/JavaText1/Text3.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class Text3
+{
+  public static void main(String[] args)
+  {
+    FileReader file = null;
+    try
+    {
+      file = new FileReader("D:\\Java\\Resources\\text3.dat");
+    } catch(FileNotFoundException fileNotFound)
+    {   
+      System.out.println("The file was not available.");
+      return;
+    }
+    Scanner fr = new Scanner(file);
+    System.out.println("================");
+    System.out.println("=Ln|     Value=");
+    System.out.println("================");
+    System.out.printf("= 1|%11s=\n= 2|%11s=\n= 3|%11s=\n", fr.nextLine(), fr.nextLine(), fr.nextLine());
+    System.out.println("================");
+    try
+    {
+      file.close();
+    } catch(IOException closeError)
+    {
+      System.out.println("There was an error closing the file. It may still be open. Closing the environment to be safe...");
+      try
+      {
+        Thread.sleep(10000);
+      } catch(InterruptedException tooSoon) {}
+      System.exit(1);
+    }
+  }
+}
