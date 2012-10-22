fe77864bde69ecc5ced68cb4c6ec943e1999e4cc
Isaac Moore <rmsy@me.com>
2012-10-21 20:04:54 -0500

    Change InOut2 to conceal personal information.

--- Solutions/JavaInOut1/InOut2.java
+++ Solutions/JavaInOut1/InOut2.java
@@ -3,7 +3,7 @@ public class InOut2
   public static void main(String[] args)
   { 
     System.out.println("Isaac Moore");
-    System.out.println("213 Whitestone Way (Weatherford, TX, 76085)");
+    System.out.println("4107 Cambridge Drive (Tempe, AZ, 85282)");
     System.out.println("(682) 710-2247");
   }
 }
