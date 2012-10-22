6deebc0057710284276a545584146edb7537108f
Isaac Moore <rmsy@me.com>
2012-10-21 20:19:49 -0500

    Create Text5 solution and accompanying resources.

---
+++ Solutions/JavaText1/Text5.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Text5
+{
+  public static void main(String[] args)
+  {
+    FileReader file = null;
+    try
+    {
+      file = new FileReader("F:\\Java\\Resources\\text5.dat");
+    } catch(FileNotFoundException fileNotFound)
+    {   
+      System.out.println("The file was not available.");
+      return;
+    }
+    Scanner fr = new Scanner(file);
+    int[] scores = new int[4];
+    scores[0] = fr.nextInt();
+    scores[1] = fr.nextInt();
+    scores[2] = fr.nextInt();
+    scores[3] = fr.nextInt();
+    System.out.printf("Tests   : %d %d %d %d\nAverage : %.3f\n", scores[0], scores[1], scores[2], scores[3], (scores[0] + scores[1] + scores[2] + scores[3]) / 4.0);
+    try
+    {
+      file.close();
+    } catch(IOException closeError)
+    {
+      System.out.println("There was an error closing the file. It may still be open. Closing the environment to be safe...");
+      try
+      {
+        Thread.sleep(10000);
+      } catch(InterruptedException tooSoon) {}
+      System.exit(1);
+    }
+  }
+}
