fbcdbad484826b3610d5410a494c2651fcac4620
Isaac Moore <rmsy@me.com>
2012-10-21 20:11:32 -0500

    Create Text4 solution and accompanying resources.

---
+++ Solutions/JavaText1/Text4.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Text4
+{
+  public static void main(String[] args)
+  {
+    FileReader file = null;
+    try
+    {
+      file = new FileReader("D:\\Java\\Resources\\text4.dat");
+    } catch(FileNotFoundException fileNotFound)
+    {   
+      System.out.println("The file was not available.");
+      return;
+    }
+    Scanner fr = new Scanner(file);
+    System.out.printf("Name    : %s\nAddress : %s\nAge     : %s\nSchool  : %s\nGPA     : %.4f\n", fr.nextLine(), fr.nextLine(), fr.nextLine(), fr.nextLine(), fr.nextDouble());
+    try
+    {
+      file.close();
+    } catch(IOException closeError)
+    {
+      System.out.println("There was an error closing the file. It may still be open. Closing the environment to be safe...");
+      try
+      {
+        Thread.sleep(10000);
+      } catch(InterruptedException tooSoon) {}
+      System.exit(1);
+    }
+  }
+}
